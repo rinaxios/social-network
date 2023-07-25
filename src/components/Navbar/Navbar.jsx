348e7c2f1851bfac7f1e10bae0dc690ebf329769
Rinat <rinaxios@gmail.com>
2023-07-25 22:00:15 +0600

    Add routing for nav menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import s from "./Navbar.module.css"
+import {NavLink} from "react-router-dom";
 
-const Navbar  = () => {
-  return (
-    <nav className={s.nav}>
-        <div className={s.item}>
-          <a href="/">Profile</a>
-        </div>
-        <div className={s.item}>
-          <a href="/">Messages</a>
-        </div>
-        <div className={s.item}>
-          <a href="/">News</a>
-        </div>
-        <div className={s.item}>
-          <a href="/">Music</a>
-        </div>
-        <div className={s.item}>
-          <a href="/">Settings</a>
-        </div>
-      </nav>
-  );
+const Navbar = () => {
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink to="/profile" className={ navData => navData.isActive ? s.active : NavLink }>Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/dialogs" className={ navData => navData.isActive ? s.active : NavLink }>Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/news" className={ navData => navData.isActive ? s.active : NavLink }>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/music" className={ navData => navData.isActive ? s.active : NavLink }>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/settings" className={ navData => navData.isActive ? s.active : NavLink }>Settings</NavLink>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
